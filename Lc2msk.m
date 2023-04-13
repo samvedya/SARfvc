1b94ae82754a3d0d1fe9e407977a3ab76e86be82
Samvedya <35847947+samvedya@users.noreply.github.com>
2023-04-13 17:50:41 +0530

    Update Lc2msk.m

--- Lc2msk.m
+++ Lc2msk.m
@@ -1,6 +1,6 @@
 function [layer1,layer2,layer3]= Lc2msk(lulc, nfeat)
 
-% Lc2msk CONVERTS EACH FEATURE IN LULC TO SEPERATE BINARY MASK
+% Lc2msk CONVERTS EACH FEATURE IN LULC AS SEPERATE BINARY MASK
 % Input LULC map should be RGB image
 % nfeat - Specify number of classes in your input LULC map
 % The output is three layer cells. Each cell stores features from
