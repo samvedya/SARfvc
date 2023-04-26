b2aac396aa39f7c14e123af827b937fa2fedd3f5
Samvedya <35847947+samvedya@users.noreply.github.com>
2023-04-26 19:37:52 +0530

    Add files via upload
    Read user input

---
+++ read_input.m
@@ -0,0 +1,60 @@
+function [lulc, flood, p_flood, pol]=read_input()
+
+% Input images should be entered in the order 
+% - LULC
+% - SAR flood image
+% - SAR pre-flood image
+% - Scattering model based classified (POLSAR) image
+
+% (C)Samvedya Surampudi@ VIT University 
+
+disp('Select data folder..............................')
+path=uigetdir('D:\');
+% Reading LULC image
+disp('Select georeferenced LULC image..................')
+[a, path] = uigetfile(fullfile(path,'*.*')) ;
+% Reading SAR flood data
+disp('Select SAR flood image..................')
+[b, path] = uigetfile(fullfile(path,'*.*')) ;
+% Reading SAR flood data
+disp('Select SAR pre-flood image ..................')
+[c, path] = uigetfile(fullfile(path,'*.*')) ;
+% Reading SAR flood data
+disp('Select georeferenced POLSAR classified image..................')
+[d, path] = uigetfile(fullfile(path,'*.*')) ;
+
+
+
+lulc = imread(a);                                        % LULC
+b = imread(b);                                           % SAR flood 
+b = medfilt2(b);                                         % Speckle filtering
+flood=(10*(log10(b.*b)))-83;                             % ALOS2 Calibration
+flood(flood==-inf)=mean(flood(find(flood~=-inf))); 
+c=imread(c);                                             % SAR Pre-flood
+c=medfilt2(c);                                           % Speckle filtering
+p_flood=(10*(log10(c.*c)))-83;                           % ALOS2 Calibration
+p_flood(p_flood==-inf)=mean(p_flood(find(p_flood~=-inf)));                                                                 
+pol=imread(d);                                         % Scattering model based classification
+
+% Equilizing dimensions of all data sets
+if (size(flood,1)<size(flood,2))
+    len=size(flood,1);
+else
+    len=size(flood,2);
+
+lulc=lulc(1:len,1:len,:);
+flood=flood(1:len,1:len);
+p_flood=p_flood(1:len,1:len);
+pol=pol(1:len,1:len);
+
+
+% Display images
+figure(1); imshow(lulc,[]);
+figure(2); imshow(flood,[]);
+figure(3); imshow(p_flood,[]);
+figure(4); cmap=rand(16,3);imshow(pol,[]); colormap (cmap); impixelinfo
+
+
+
+end
+
